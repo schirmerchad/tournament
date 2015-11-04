a23adc8ebb4e036530939f68d2e316736d581ca4
Chad <schirmerchad@gmail.com>
2015-11-03 20:35:11 -0600

    Add database creation and connection sql

--- tournament.sql
+++ tournament.sql
@@ -6,6 +6,10 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
+
 DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS matches;
 DROP VIEW IF EXISTS winCount;
@@ -13,10 +17,11 @@ DROP VIEW IF EXISTS lossCount;
 DROP VIEW IF EXISTS standings;
 
 CREATE TABLE players 
-(id SERIAL PRIMARY KEY, name TEXT);
+(id SERIAL PRIMARY KEY, name TEXT NOT NULL);
 
 CREATE TABLE matches 
-(match_id SERIAL, winner INTEGER REFERENCES players(id), loser INTEGER REFERENCES players(id));
+(match_id SERIAL, winner INT REFERENCES players(id) ON DELETE CASCADE, loser INT REFERENCES players(id) ON DELETE CASCADE)
+CHECK (winner <> loser);
 
 CREATE VIEW winCount 
 AS SELECT id, name, COUNT(winner) AS wins 
@@ -27,16 +32,6 @@ CREATE VIEW lossCount
 AS SELECT id, name, COUNT(loser) AS losses 
 FROM players LEFT JOIN matches 
 ON players.id = matches.loser GROUP BY id;
-
---CREATE VIEW matchCount 
---AS SELECT id, name, COUNT(id) AS matches 
---FROM players LEFT JOIN matches 
---ON players.id = matches.winner or players.id = matches.loser GROUP BY id;
-
---CREATE VIEW standings 
---AS SELECT winCount.id, winCount.name, wins, matches 
---FROM winCount LEFT JOIN matchCount 
---ON winCount.id = matchCount.id ORDER BY wins DESC;
 
 CREATE VIEW standings
 AS SELECT winCount.id, winCount.name, wins, (winCount.wins + lossCount.losses) as matches
