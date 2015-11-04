e344253679fe9e014c324ba985218af2258afe44
Chad <schirmerchad@gmail.com>
2015-11-04 07:04:34 -0600

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -10,30 +10,35 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament;
 
-DROP TABLE IF EXISTS players;
-DROP TABLE IF EXISTS matches;
-DROP VIEW IF EXISTS winCount;
-DROP VIEW IF EXISTS lossCount;
-DROP VIEW IF EXISTS standings;
-
 CREATE TABLE players 
 (id SERIAL PRIMARY KEY, name TEXT NOT NULL);
 
 CREATE TABLE matches 
-(match_id SERIAL, winner INT REFERENCES players(id) ON DELETE CASCADE, loser INT REFERENCES players(id) ON DELETE CASCADE)
-CHECK (winner <> loser);
+  (match_id SERIAL,
+  winner INT REFERENCES players(id) ON DELETE CASCADE,
+  loser INT REFERENCES players(id) ON DELETE CASCADE,
+  CHECK (winner <> loser));
 
 CREATE VIEW winCount 
 AS SELECT id, name, COUNT(winner) AS wins 
 FROM players LEFT JOIN matches 
 ON players.id = matches.winner GROUP BY id;
 
-CREATE VIEW lossCount 
-AS SELECT id, name, COUNT(loser) AS losses 
-FROM players LEFT JOIN matches 
-ON players.id = matches.loser GROUP BY id;
+CREATE VIEW lossCount AS
+  SELECT id,
+        name,
+        COUNT(loser) AS losses 
+  FROM players
+  LEFT JOIN matches 
+    ON players.id = matches.loser
+    GROUP BY id;
 
-CREATE VIEW standings
-AS SELECT winCount.id, winCount.name, wins, (winCount.wins + lossCount.losses) as matches
-FROM winCount LEFT JOIN lossCount
-ON winCount.id = lossCount.id ORDER BY wins DESC;
+CREATE VIEW standings AS
+  SELECT winCount.id,
+         winCount.name,
+         wins,
+         (winCount.wins + lossCount.losses) as matches
+  FROM winCount
+  LEFT JOIN lossCount
+    ON winCount.id = lossCount.id
+  ORDER BY wins DESC;
